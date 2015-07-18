06a6673d18e617fec447ae3dffe737734991575c
Ryan Killgore <rekillgore@gmail.com>
2015-07-18 14:00:02 -0700

    Add files and folders for navmesh and agents

---
+++ Assets/scripts/AI/Spawner.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+namespace TDL {
+    class Spawner : MonoBehaviour {
+        public int minionCount = 0;
+        public float waitTime = 0f;
+
+        public UnityEngine.Object prefab = null;
+
+        private float _lastSpawnTime = 0f;
+        private int _numSpawned = 0;
+
+        void Awake() {
+            _lastSpawnTime = Time.time;
+        }
+
+        void Start() {
+
+        }
+
+        void Update() {
+            if (Time.time - _lastSpawnTime > waitTime && _numSpawned < minionCount) {
+                _numSpawned++;
+                _lastSpawnTime = Time.time;
+                Debug.Log("SPAWN! "+_numSpawned.ToString());
+                GameObject minion = (GameObject)GameObject.Instantiate(prefab);
+                minion.transform.position = this.transform.position;
+            }
+        }
+
+    }
+}
